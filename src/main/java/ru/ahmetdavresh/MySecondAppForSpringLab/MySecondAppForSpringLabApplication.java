1894886acaf9de794fd89a9d98fd00c7d10a49cd
ahmet <ahmetdavresh@gmail.com>
2023-10-21 09:07:17 +0300

    init

---
+++ src/main/java/ru/ahmetdavresh/MySecondAppForSpringLab/MySecondAppForSpringLabApplication.java
@@ -0,0 +1,13 @@
+package ru.ahmetdavresh.MySecondAppForSpringLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecondAppForSpringLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySecondAppForSpringLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ahmetdavresh/MySecondAppForSpringLab/MySecondAppForSpringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ahmetdavresh.MySecondAppForSpringLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondAppForSpringLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
