662806329ed2a1814254c57ca5c41376767d0334
ahmet <ahmetdavresh@gmail.com>
2023-10-25 17:55:12 +0300

    complete 3.2 test version1

--- src/main/java/ru/ahmetdavresh/MySecondAppForSpringLab/service/ValidationService.java
+++ src/main/java/ru/ahmetdavresh/MySecondAppForSpringLab/service/ValidationService.java
@@ -1,10 +1,10 @@
 package ru.ahmetdavresh.MySecondAppForSpringLab.service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import ru.ahmetdavresh.MySecondAppForSpringLab.exception.ValidationFailedException;
 
-import javax.naming.Binding;
-
+@Service
 public interface ValidationService {
     void isValid(BindingResult bindingResult) throws ValidationFailedException;
 }
